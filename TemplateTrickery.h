6ccb7748ee1cbacaba5db1bd85d58edac99031f1
isaac868 <33269071+isaac868@users.noreply.github.com>
2019-10-04 16:39:02 -0700

    Update TemplateTrickery.h
    More Templates

--- TemplateTrickery.h
+++ TemplateTrickery.h
@@ -50,10 +50,41 @@ using TRAIT_NAME = TRAIT_TYPE_HELPERS::TRAIT_NAME##_helper<T, ReturnType, void, 
 template <typename T, typename ReturnType, typename ... ARGS>                       \
 constexpr bool TRAIT_NAME##_v = TRAIT_NAME<T, ReturnType, ARGS...>::value;
 
+
+//Helpers to encapulate types in values and extract them from values
+template <typename T>
+struct TypeT
+{
+   using Type = T;
+};
+
+template <typename T>
+constexpr TypeT<T> type = TypeT<T>{};
+
+template<typename T>
+T valueT(TypeT<T>);
+
+
 //Checks if test is of type T*
 template <typename T, typename U>
 bool is_type_of(U* test)
 {
    T tester{};
    return *(void**)&tester == *(void**)test;
+}
+
+//Checks if the object T hac the paren operator that takes ARGS
+template <typename T, typename = void, typename ... ARGS>
+struct isValid_Impl : std::false_type {};
+
+template <typename T, typename ... ARGS>
+struct isValid_Impl <T, std::void_t<decltype(std::declval<T>()(std::declval<ARGS&&>()...))>, ARGS...> : std::true_type {};
+
+//Modification of isValid from C++ Templates - The Complete guide
+//http://www.tmplbook.com/code/traits/isvalid.hpp.html
+template <typename T>
+constexpr auto is_valid(T f) {
+   return [](auto&&... args) {
+      return isValid_Impl<T, void, decltype(args) && ...>::value;
+   };
 }
